6922daadbab39c675ede625a20fda882c1270f0e
aiexplorer1000 <38206778-aiexplorer1000@users.noreply.replit.com>
2025-01-09 13:14:00 +0000

    Agent query: Применился ли шрифт Inter на сайте?
    Update font to Inter Variable.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/e22086b2-38f4-4600-89b8-4d0e7c29c1ba/fee82b8e-6eec-4ad9-9dd3-a019b9400e82.jpg

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,7 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
 import App from './App';
-import "@fontsource-variable/geist";
+import "@fontsource-variable/inter";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
